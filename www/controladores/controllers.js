69f65e3ff4a703752e2cd038fd5dd3b0e3f99d60
Nestor <jokera666@gmail.com>
2016-09-30 19:49:45 +0200

    Calendario Añadido falta pulir

--- www/controladores/controllers.js
+++ www/controladores/controllers.js
@@ -130,14 +130,178 @@ angular.module('starterMiApp.controllers', [])
 
 }]) // Fin SidemenuCtrl
 
-.controller('AgendaCtrl', ['$scope', '$http', '$state','$stateParams', function($scope, $http, $state,$stateParams){
+//.controller('AgendaCtrl', ['$scope', '$http', '$state','$stateParams', function($scope, $http, $state,$stateParams){
+.controller('AgendaCtrl',['$scope', '$http', '$state','$stateParams','$ionicModal','uiCalendarConfig', function($scope, $compile, $timeout, uiCalendarConfig,$ionicModal) {
 
-     console.log('entro 1');
-     // console.log($stateParams);
-     // $scope.nombreUsuario = $stateParams.param1;
-     // $scope.contrasena = $stateParams.param2;
-     console.log($scope.objetos); 
-     $scope.nombreUsuario = $scope.objetos;
+
+    console.log($scope.objetos); 
+    $scope.nombreUsuario = $scope.objetos;
+    
+    var date = new Date();
+    var d = date.getDate();
+    var m = date.getMonth();
+    var y = date.getFullYear();
+
+    $scope.changeTo = 'Hungarian';
+    /* event source that pulls from google.com */
+    $scope.eventSource = {
+            //url: "http://www.google.com/calendar/feeds/usa__en%40holiday.calendar.google.com/public/basic",
+            className: 'gcal-event',           // an option!
+            currentTimezone: 'Europe/Madrid' // an option!
+    };
+    /* event source that contains custom events on the scope */
+    $scope.events = [
+      {title: 'All Day Event',start: new Date(y, m, 1)},
+      {title: 'Long Event',start: new Date(y, m, d - 5),end: new Date(y, m, d - 2)},
+      {id: 999,title: 'Repeating Event',start: new Date(y, m, d - 3, 16, 0),allDay: false},
+      {id: 999,title: 'Repeating Event',start: new Date(y, m, d + 4, 16, 0),allDay: false},
+      {title: 'Birthday Party',start: new Date(y, m, d + 1, 19, 0),end: new Date(y, m, d + 1, 21, 30),allDay: false},
+      {title: 'Click for Google',start: new Date(y, m, 28, 09, 15),end: new Date(y, m, 28, 10, 25),url: 'http://google.com/'}
+    ];
+    /* event source that calls a function on every view switch */
+    $scope.eventsF = function (start, end, timezone, callback) {
+      var s = new Date(start).getTime() / 1000;
+      var e = new Date(end).getTime() / 1000;
+      var m = new Date(start).getMonth();
+      var events = [{title: 'Feed Me ' + m,start: s + (50000),end: s + (100000),allDay: false, className: ['customFeed']}];
+      callback(events);
+    };
+
+    $scope.calEventsExt = {
+       color: '#f00',
+       textColor: 'yellow',
+       events: [ 
+          {type:'party',title: 'Lunch',start: new Date(y, m, d, 12, 0),end: new Date(y, m, d, 14, 0),allDay: false},
+          {type:'party',title: 'Lunch 2',start: new Date(y, m, d, 12, 0),end: new Date(y, m, d, 14, 0),allDay: false},
+          {type:'party',title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
+        ]
+    };
+    /* alert on eventClick */
+    $scope.alertOnEventClick = function( date, jsEvent, view){
+        $scope.alertMessage = (date.title + ' was clicked ');
+        date.start = new Date();
+        console.log('entro');
+        alert(date.title+' fue pulsada '+date.start);
+    };
+    /* alert on Drop */
+     $scope.alertOnDrop = function(event, delta, revertFunc, jsEvent, ui, view){
+      console.log('entro');
+       $scope.alertMessage = ('Event Dropped to make dayDelta ' + delta);
+    };
+    /* alert on Resize */
+    $scope.alertOnResize = function(event, delta, revertFunc, jsEvent, ui, view ){
+       $scope.alertMessage = ('Event Resized to make dayDelta ' + delta);
+    };
+    /* add and removes an event source of choice */
+    $scope.addRemoveEventSource = function(sources,source) {
+      var canAdd = 0;
+      angular.forEach(sources,function(value, key){
+        if(sources[key] === source){
+          sources.splice(key,1);
+          canAdd = 1;
+        }
+      });
+      if(canAdd === 0){
+        sources.push(source);
+      }
+    };
+    /* add custom event*/
+    $scope.addEvent = function() {
+      $scope.events.push({
+        title: 'Cita a Maria Jose',
+        start: new Date(y, m, d, 08, 00),
+        end: new Date(y, m, d, 08, 30),
+        className: ['openSesame']
+      });
+    };
+    /* remove event */
+    $scope.remove = function(index) {
+      $scope.events.splice(index,1);
+    };
+    /* Change View */
+    $scope.changeView = function(view,calendar) {
+      uiCalendarConfig.calendars[calendar].fullCalendar('changeView',view);
+    };
+    /* Change View */
+    $scope.renderCalendar = function(calendar) {
+      $timeout(function() {
+        if(uiCalendarConfig.calendars[calendar]){
+          uiCalendarConfig.calendars[calendar].fullCalendar('render');
+        }
+      });
+    };
+     /* Render Tooltip */
+    // $scope.eventRender = function( event, element, view ) {
+    //     element.attr({'tooltip': event.title,
+    //                   'tooltip-append-to-body': true});
+    //     $compile(element)($scope);
+    // };
+
+      $scope.uiConfig = {
+        calendar: {
+            lang: 'es',
+            locale: 'es',
+            height: 550,
+            editable: false,
+            droppable: false,
+            allDaySlot: false,
+            titleFormat: 'DD MMMM YYYY',
+            slotLabelFormat: 'HH:mm',
+            slotDuration: '00:15:00',
+            defaultView: 'agendaDay',
+            minTime: '08:00',
+            maxTime: '21:00',
+            drop: $scope.drop,
+            header: {
+                left: 'list month basicDay agendaWeek',
+                center: 'title',
+                right: 'agendaDay prev,next'
+            },
+            buttonText: {
+              // prev: 'Anterior',
+              // next: 'Siguiente',
+              // prevYear: '&nbsp;&lt;&lt;&nbsp;',
+              // nextYear: '&nbsp;&gt;&gt;&nbsp;',
+              today:  'Hoy',
+              month:  'Mes',
+              week:   'Semana',
+              day:    'Dia Actual'
+          },
+            eventClick: $scope.alertOnEventClick,
+            eventDrop: $scope.alertOnDrop,
+            eventResize: $scope.alertOnResize
+            //eventRender: $scope.eventRender
+        }
+    };
+
+    $scope.changeLang = function() {
+      if($scope.changeTo === 'Hungarian'){
+        $scope.uiConfig.calendar.dayNames = ["Vasárnap", "Hétfő", "Kedd", "Szerda", "Csütörtök", "Péntek", "Szombat"];
+        $scope.uiConfig.calendar.dayNamesShort = ["Vas", "Hét", "Kedd", "Sze", "Csüt", "Pén", "Szo"];
+        $scope.changeTo= 'English';
+      } else {
+        $scope.uiConfig.calendar.dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        $scope.uiConfig.calendar.dayNamesShort = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+        $scope.changeTo = 'Hungarian';
+      }
+    };
+    /* event sources array*/
+    $scope.eventSources = [$scope.events, $scope.eventSource, $scope.eventsF];
+    $scope.eventSources2 = [$scope.calEventsExt, $scope.eventsF, $scope.events];
+
+    // 'plantillas/modalInsertarCliente.html' URL para ejecutar en el movil
+    $ionicModal.fromTemplateUrl('plantillas/modalInsertarCita.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.modal = modal;
+    });
+    $scope.openModal = function() {
+      $scope.modal.show();
+    };
+    $scope.closeModal = function() {
+      $scope.modal.hide();
+    };
 
 }]) // Fin AgendaCtrl
 
