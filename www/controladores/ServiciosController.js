6d7fe7ab26b6241e79e7bfd1e41a43d5d141d453
Nestor <jokera666@gmail.com>
2016-12-08 16:31:47 +0100

    Añadir servicios en produccion

--- www/controladores/ServiciosController.js
+++ www/controladores/ServiciosController.js
@@ -2,4 +2,25 @@ angular.module('starterMiApp.contrsServicios', [])
 
 .controller('ServiciosCtrl', ['$scope','$state','$stateParams', function($scope,$state,$stateParams){
 
+    $scope.colors = [
+      {name:'black', shade:'dark'},
+      {name:'white', shade:'light', notAnOption: true},
+      {name:'red', shade:'dark'},
+      {name:'blue', shade:'dark', notAnOption: true},
+      {name:'yellow', shade:'light', notAnOption: false}
+    ];
+
+	$scope.items = [
+		{servicio:'Corte',seccion:'Peluqueria'},
+		{servicio:'Color',seccion:'Peluqueria'},
+		{servicio:'Moldeador',seccion:'Peluqueria'}
+	];
+
+	$scope.getSeccion = function(seccion)
+	{
+		console.log(seccion);
+		alert(seccion.name);
+	}
+
+
 }]) // Fin ServiciosCtrl
