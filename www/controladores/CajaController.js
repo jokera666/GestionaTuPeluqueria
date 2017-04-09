43c51acbb02827e7ed08507268d686c0647b1a6e
Nestor <jokera666@gmail.com>
2017-04-09 21:06:25 +0200

    Falta pulir vista Caja

--- www/controladores/CajaController.js
+++ www/controladores/CajaController.js
@@ -5,4 +5,45 @@ angular.module('starterMiApp.contrsCaja', [])
 	console.log($stateParams.idCita);
 	console.log($stateParams.fechaCita);
 
+	// $scope.nombreCliente = 'Nestor';
+
+	$scope.empleados = [
+		{nombre:'Jose', apellidos: 'Perez'},
+		{nombre:'Manuel', apellidos: 'Perez'},
+		{nombre:'Maria', apellidos: 'Perez'}
+	];
+
+	$scope.secciones = [
+		{nombre:'Peluquería'},
+		{nombre:'Estetica'},
+		{nombre:'Manicura'}
+	];
+
+	$scope.servicios = [
+		{nombre:'Corte'},
+		{nombre:'Tinte'},
+		{nombre:'Moldeador'}
+	];
+
+	$scope.categorias = [
+		{nombre:'Hombre'},
+		{nombre:'Mujer'},
+		{nombre:'Niño'}
+	];
+
+
+	$scope.todoListServicios = [];
+
+
+
+	$scope.anadirServicio  = function()
+	{
+		$scope.todoListServicios.push({});
+	};
+
+	$scope.eliminarServicio = function (index)
+  {
+        $scope.todoListServicios.splice(index, 1);
+  };
+
 }]) // Fin CajaCtrl
